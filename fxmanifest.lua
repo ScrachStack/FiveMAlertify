40975e2ab9befec944275b239a460e8b92318d1d
KeziCodesSometimes <122732007+KeziCodesSometimes@users.noreply.github.com>
2023-05-15 03:05:48 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Kezi'
+description 'Noti'
+version '1.0.0'
+
+client_scripts {
+    'client.lua',
+}
+
+ui_page 'index.html'
+
+files {
+    'index.html',
+}
+
